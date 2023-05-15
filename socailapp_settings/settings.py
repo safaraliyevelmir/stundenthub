53db53c649cc593576535d832f3ad0491f78121f
safaraliyevelmir <safaraliyevelmir@gmail.com>
2023-05-15 17:35:11 +0100

    heroku

--- socailapp_settings/settings.py
+++ socailapp_settings/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
